7cac0cf55eea5b259356764c50aa72fbde2f7f96
BlackBeard1O1 <henos.job@gmail.com>
2023-02-28 02:05:15 +0000

    CompareTwoArrays Lab

--- src/main/java/ArraysAreEqual.java
+++ src/main/java/ArraysAreEqual.java
@@ -15,6 +15,18 @@ public class ArraysAreEqual {
      * @return true if the values of a are equal to the values of b.
      */
     public boolean equal(int[] a, int[] b){
-        return false;
+        boolean bool = true;
+        for(int x: a){
+            for(int i=0; i<a.length; i++){
+                if(a[i] != b[i]){
+                    bool = false;
+                    break;
+                }
+                else
+                bool = true;
+            }
+        }
+
+        return bool;
     }
 }
